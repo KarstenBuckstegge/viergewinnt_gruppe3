0aa99dc0e5d99fa70f0d471dc28e451431965a59
Christian Samide <samidemail@gmail.com>
2013-03-07 22:23:43 +0100

    Connect, WriteFile, Test

---
+++ src/connect/Test.java
@@ -0,0 +1,23 @@
+package connect;
+
+import java.io.IOException;
+
+public class Test {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Test gestartet!");
+		
+		Connect.setStone(5);
+		
+		System.out.println("Test beendet, jetzt die Textdatei im Stammverzeichnis prüfen!");
+		
+		
+		
+	}
+
+}

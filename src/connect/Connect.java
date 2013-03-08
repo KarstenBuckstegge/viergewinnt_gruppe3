48a630cb7751dfd15297ba71741ab30a2812f9a2
Christian Samide <samidemail@gmail.com>
2013-03-08 08:20:05 +0100

    Create Connect.java

---
+++ src/connect/Connect.java
@@ -0,0 +1,33 @@
+package connect;
+
+import java.io.IOException;
+
+public class Connect {
+
+  /**
+	 * @param args
+	 */
+	// Kommentar
+	int opMove = 4; // Simuliert Info aus Server-Datei
+	static int move = 3; // Stellvertretend für letzten Zug
+	public static void main(String[] args)
+	
+	{
+		System.out.println("Connect gestartet!");
+	}
+	
+	static public void setStone(int column) throws IOException
+	{
+		move = column;
+		WriteFile.main(null);
+		System.out.println("Connect.setStone aufgerufen!");
+	}
+	
+     
+	
+		
+	
+	
+	
+
+}

01f4afc70f57042906b53ad7323a111183f1abdc
Christian Samide <samidemail@gmail.com>
2013-03-15 10:18:26 +0100

    JDOM

--- src/connect/Connect.java
+++ src/connect/Connect.java
@@ -5,9 +5,28 @@ import java.io.IOException;
 public class Connect {
 
   /**
+   * Klasse Connect stellt die Schnittstelle zwischen Server und Agent dar.
+   * Hauptprogramm für alle Kommunikationsabläufe
+   * 
 	 * @param args
 	 */
-	// Kommentar
+
+	// 1. Parameter holen
+	// - Von GUI-Dropdown ermitteln ob man spielero oder spielerx ist.
+	// - Laufwerk aus GUI-Browsing-Dialog ziehen.	
+
+	// 2. Funktion: Serverfile von Kontaktpfad lesen.
+	
+	ReadJDOM read = new ReadJDOM();
+	
+	System.out.println(read.getEnemyMove());
+	
+
+	// 3. Funktion: Agentfile auf Kontaktpfad schreiben.
+	
+		
+	
+	
 	int opMove = 4; // Simuliert Info aus Server-Datei
 	static int move = 3; // Stellvertretend f√ºr letzten Zug
 	public static void main(String[] args)

26ab332c2ef82e0e99280fbf4512ab428f4d2f33
MrBambule <k.buckstegge@googlemail.com>
2013-03-26 12:44:35 +0100

    - Siege in Zeilen werden erkannt

--- src/logic/TestKI.java
+++ src/logic/TestKI.java
@@ -18,15 +18,16 @@ public class TestKI {
 		int kempColumn = tempColumn+3;
 		
 		field[tempColumn][tempRow] = 1;
-		field[lempColumn][tempRow] = 1;
+		field[lempColumn][tempRow] = 0;
 		field[mempColumn][tempRow] = 1;
-		field[kempColumn][tempRow] = 2;
+		field[kempColumn][tempRow] = 1;
 		
 		int startValue = field[tempColumn][tempRow];
 		int leftStartValue = field[tempColumn - 1][tempRow];
 		int winCounter = 0;
 		int gap = 0;
 		int gapPosition = 0;
+		int winColumn = -1;
 		
 		for (int i = 0; i<4; i++) {
 			
@@ -51,27 +52,23 @@ public class TestKI {
 				break;
 			}
 			// befindet sich im aktuellen Feld ein anderer Stein wird die Schleife unterbrochen
-			else {
+			else if (winCounter < 3 && tempValue != startValue && tempValue != 0) {
 				System.out.println("column: " + checkColumn + " value: " + field[checkColumn][tempRow] + " BREAK");
 				break;
 			}
 			
-			// erkennt Sieg bei offenem Feld an vierter Spalte
-			if (winCounter == 3 && tempValue == 0){
-				System.out.println("Win at Row " + tempColumn + " + 4");
-				return tempColumn + 4;
-			}
-			
 			// erkennt Sieg bei offenem Feld an zweiter oder dritter Stelle
 			if (winCounter == 3 && gap == 1){
-				System.out.println("Win at Row " + tempColumn + " + " + gapPosition);
-				return tempColumn + gapPosition;
+				winColumn = tempColumn + gapPosition;
+				System.out.println("Win at Column " + winColumn);
+				return winColumn;
 			}
 			
 			// erkennt Sieg bei offenem Feld vor der ersten Stelle
-			if (tempColumn > 0 && i == 4 && winCounter == 3 && leftStartValue == 0){
-				System.out.println("Win at Row " + tempColumn + " - 1 ");
-				return tempColumn - 1;
+			if (tempColumn > 0 && i >= 3 && winCounter == 3 && leftStartValue == 0){
+				winColumn = tempColumn - 1;
+				System.out.println("Win at Column " + winColumn);
+				return winColumn;
 			}
 			
 		}
